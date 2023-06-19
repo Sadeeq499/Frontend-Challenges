8600691d9678caa5a0fc1240ca3d5f665e1d015c
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2023-06-19 12:54:14 +0500

    all mange page completed with responsiveness

--- managelandingpage/src/Components/MainLayout/Header.jsx
+++ managelandingpage/src/Components/MainLayout/Header.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Images from "../utility/Constrants/Images";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToggle = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
     <>
       {/* navbar */}
@@ -14,7 +20,6 @@ function Header() {
           </div>
 
           {/* Menu items */}
-
           <div className="hidden md:flex items-center space-x-6">
             <a className="hover:text-DarkGrayishBlue" href="#pricing">
               {" "}
@@ -42,33 +47,45 @@ function Header() {
           <button className="hidden md:block rounded-full px-6 py-2 text-white bg-BrightRed  hover:bg-BrightRed  hover:opacity-80">
             Get Started
           </button>
+
           {/* Hamburger */}
+          <div className="items-end md:hidden">
+            <button onClick={handleToggle} className="w-16 h-16">
+              {showMenu ? (
+                <img src={Images.Close} alt="close " />
+              ) : (
+                <img src={Images.Hamburger} alt="hamburger" />
+              )}
+            </button>
+          </div>
 
           {/* mobile view */}
-          <div className="md:hidden">
-            <div className="absolute flex flex-col hidden items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
-              <a className="hover:text-DarkGrayishBlue" href="#pricing">
-                {" "}
-                Pricing{" "}
-              </a>
-              <a className="hover:text-DarkGrayishBlue" href="#product">
-                {" "}
-                Product{" "}
-              </a>
-              <a className="hover:text-DarkGrayishBlue" href="#about">
-                {" "}
-                About{" "}
-              </a>
-              <a className="hover:text-DarkGrayishBlue" href="#contact">
-                {" "}
-                Contact{" "}
-              </a>
-              <a className="hover:text-DarkGrayishBlue" href="#contact">
-                {" "}
-                Contact{" "}
-              </a>
+          {showMenu && (
+            <div className="md:hidden">
+              <div className="absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+                <a className="hover:text-DarkGrayishBlue" href="#pricing">
+                  {" "}
+                  Pricing{" "}
+                </a>
+                <a className="hover:text-DarkGrayishBlue" href="#product">
+                  {" "}
+                  Product{" "}
+                </a>
+                <a className="hover:text-DarkGrayishBlue" href="#about">
+                  {" "}
+                  About{" "}
+                </a>
+                <a className="hover:text-DarkGrayishBlue" href="#contact">
+                  {" "}
+                  Contact{" "}
+                </a>
+                <a className="hover:text-DarkGrayishBlue" href="#contact">
+                  {" "}
+                  Contact{" "}
+                </a>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </nav>
     </>
